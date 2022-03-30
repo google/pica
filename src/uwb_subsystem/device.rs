e952e755765c54cb606778b6e6841da6d18b2741
Henri Chataing <henrichataing@google.com>
2022-03-30 15:34:49 +0200

    Move device and session command implementation to corresponding modules
    Change-Id: I2b85125c9998a2e421af177bdf529a5a7af1851e

--- src/uwb_subsystem/device.rs
+++ src/uwb_subsystem/device.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
-use crate::uci_packets::{DeviceState, SessionState, UciPacketPacket};
+use crate::uci_packets::{DeviceState, UciPacketPacket};
+use crate::uwb_subsystem::*;
+
 use anyhow::{anyhow, Result};
 use tokio::sync::mpsc;
 use crate::position::Position;
@@ -38,5 +40,101 @@ impl Device {
             session
         );
         Ok(())
+    }
+}
+
+impl Pica {
+    // The fira norm specify to send a response, then reset, then
+    // send a notification once the reset is done
+    pub async fn device_reset(
+        &mut self,
+        device_handle: usize,
+        cmd: DeviceResetCmdPacket,
+    ) -> Result<()> {
+        let reset_config = cmd.get_reset_config();
+        println!("[{}] Device Reset", device_handle);
+        println!("  reset_config={}", reset_config);
+        {
+            let mut device = self.get_device(device_handle);
+            let status = match reset_config {
+                ResetConfig::UwbsReset => StatusCode::UciStatusOk,
+            };
+            device.state = DeviceState::DeviceStateReady;
+            device
+                .tx
+                .send(DeviceResetRspBuilder { status }.build().into())
+                .await?;
+        }
+
+        self.devices.insert(
+            device_handle,
+            Device::new(device_handle, self.devices[&device_handle].tx.clone()),
+        );
+        Ok(self
+            .get_device(device_handle)
+            .tx
+            .send(
+                DeviceStatusNtfBuilder {
+                    device_state: DeviceState::DeviceStateReady,
+                }
+                .build()
+                .into(),
+            )
+            .await?)
+    }
+
+    pub async fn get_device_info(
+        &mut self,
+        device_handle: usize,
+        _cmd: GetDeviceInfoCmdPacket,
+    ) -> Result<()> {
+        // TODO: Implement a fancy build time state machine instead of crash at runtime
+        let device = self.get_device(device_handle);
+        assert_eq!(device.state, DeviceState::DeviceStateReady);
+        Ok(device
+            .tx
+            .send(
+                GetDeviceInfoRspBuilder {
+                    status: StatusCode::UciStatusOk,
+                    uci_version: 1,
+                    mac_version: 1,
+                    phy_version: 1,
+                    uci_test_version: 1,
+                    vendor_spec_info: Vec::new(),
+                }
+                .build()
+                .into(),
+            )
+            .await?)
+    }
+
+    pub async fn get_caps_info(
+        &mut self,
+        _device_handle: usize,
+        _cmd: GetCapsInfoCmdPacket,
+    ) -> Result<()> {
+        todo!()
+    }
+
+    pub async fn set_config(&mut self, device_handle: usize, _cmd: SetConfigCmdPacket) -> Result<()> {
+        let device = self.get_device(device_handle);
+        assert_eq!(device.state, DeviceState::DeviceStateReady);
+        // TODO: Check if the config is supported
+        // Currently we are saying we support everything
+        Ok(device
+            .tx
+            .send(
+                SetConfigRspBuilder {
+                    status: StatusCode::UciStatusOk,
+                    cfg_status: Vec::new(),
+                }
+                .build()
+                .into(),
+            )
+            .await?)
+    }
+
+    pub async fn get_config(&mut self, _device_handle: usize, _cmd: GetConfigCmdPacket) -> Result<()> {
+        todo!()
     }
 }
--- src/uwb_subsystem/session.rs
+++ src/uwb_subsystem/session.rs
@@ -1,5 +1,8 @@
-use crate::uci_packets::{SessionState, SessionType};
+
 use tokio::task::JoinHandle;
+
+use crate::uci_packets::{SessionState, SessionType};
+use crate::uwb_subsystem::*;
 
 pub const MAX_SESSION: usize = 255;
 
@@ -22,5 +25,117 @@ impl Default for Session {
             ranging_interval: 0,
             ranging_task: None,
         }
+    }
+}
+
+impl Pica {
+    pub async fn session_init(
+        &mut self,
+        device_handle: usize,
+        cmd: SessionInitCmdPacket,
+    ) -> Result<()> {
+        println!("session_init");
+        let device = self.get_device(device_handle);
+        let mut session = Session::default();
+        session.state = SessionState::SessionStateInit;
+        session.id = cmd.get_session_id();
+        session.session_type = cmd.get_session_type();
+        let status = match device.add_session(session) {
+            Ok(_) => StatusCode::UciStatusOk,
+            Err(_) => StatusCode::UciStatusFailed,
+        };
+        device
+            .tx
+            .send(SessionInitRspBuilder { status: status }.build().into())
+            .await?;
+        Ok(device
+            .tx
+            .send(
+                SessionStatusNtfBuilder {
+                    session_id: cmd.get_session_id(),
+                    session_state: SessionState::SessionStateInit,
+                    reason_code: ReasonCode::StateChangeWithSessionManagementCommands,
+                }
+                .build()
+                .into(),
+            )
+            .await?)
+    }
+
+    pub async fn session_deinit(
+        &mut self,
+        _device_handle: usize,
+        _cmd: SessionDeinitCmdPacket,
+    ) -> Result<()> {
+        todo!()
+    }
+
+    pub async fn session_set_app_config(
+        &mut self,
+        device_handle: usize,
+        cmd: SessionSetAppConfigCmdPacket,
+    ) -> Result<()> {
+        // TODO: Set session app configuration regardings the incoming cmd
+        let device = self.get_device(device_handle);
+        let session = device.sessions.get_mut(&cmd.get_session_id()).unwrap();
+        assert_eq!(session.state, SessionState::SessionStateInit);
+        println!("session_set_app_config");
+        session.state = SessionState::SessionStateIdle;
+        device
+            .tx
+            .send(
+                SessionSetAppConfigRspBuilder {
+                    status: StatusCode::UciStatusOk,
+                    cfg_status: Vec::new(),
+                }
+                .build()
+                .into(),
+            )
+            .await?;
+
+        Ok(device
+            .tx
+            .send(
+                SessionStatusNtfBuilder {
+                    session_id: cmd.get_session_id(),
+                    session_state: SessionState::SessionStateIdle,
+                    reason_code: ReasonCode::StateChangeWithSessionManagementCommands,
+                }
+                .build()
+                .into(),
+            )
+            .await?)
+    }
+
+    pub async fn session_get_app_config(
+        &mut self,
+        _device_handle: usize,
+        _cmd: SessionGetAppConfigCmdPacket,
+    ) -> Result<()> {
+        todo!()
+    }
+
+    pub async fn session_get_count(
+        &mut self,
+        _device_handle: usize,
+        _cmd: SessionGetCountCmdPacket,
+    ) -> Result<()> {
+        todo!()
+    }
+
+    pub async fn session_get_state(
+        &mut self,
+        _device_handle: usize,
+        _cmd: SessionGetStateCmdPacket,
+    ) -> Result<()> {
+        todo!()
+    }
+
+    pub async fn session_update_controller_multicast_list(
+        &mut self,
+        _device_handle: usize,
+        _cmd: SessionUpdateControllerMulticastListCmdPacket,
+    ) -> Result<()> {
+        todo!()
     }
 }
