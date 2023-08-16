0437c3959dfeb9cbca9e98e566a8a3cfe27accee
ziyiw <ziyiw@google.com>
2023-08-16 16:44:59 +0000

    Handle adding/removing controlee cmd.

--- src/session.rs
+++ src/session.rs
@@ -741,18 +741,85 @@ impl Session {
                 || (self.app_config.multi_node_mode != MultiNodeMode::OneToMany
                     && self.app_config.multi_node_mode != MultiNodeMode::ManyToMany)
             {
-                StatusCode::UciStatusRejected
+                return SessionUpdateControllerMulticastListRspBuilder {
+                    status: StatusCode::UciStatusRejected,
+                }
+                .build();
             } else {
                 let action = UpdateMulticastListAction::from_u8(cmd.get_action().into()).unwrap();
+                let mut dst_addresses = self.app_config.dst_mac_addresses.clone();
+                let packet =
+                    SessionUpdateControllerMulticastListCmdPayload::parse(cmd.get_payload())
+                        .unwrap();
+                let new_controlees = packet.controlees;
+                let mut controlee_status = Vec::new();
 
-                if action == UpdateMulticastListAction::Add
-                // TODO: Check whether the amount of controlee exceed the max number
-                {
-                    StatusCode::UciStatusMulticastListFull
-                } else {
-                    // TODO properly Add/Remove the controlees and send notif (?)
-                    StatusCode::UciStatusOk
+                let session_id = self.id;
+                let mut status = StatusCode::UciStatusOk;
+                match action {
+                    UpdateMulticastListAction::Add => {
+                        new_controlees.iter().for_each(|controlee| {
+                            let mut update_status =
+                                MulticastUpdateStatusCode::StatusOkMulticastListUpdate;
+                            if !dst_addresses.contains(&MacAddress::Short(controlee.short_address))
+                            {
+                                if dst_addresses.len() == MAX_NUMBER_OF_CONTROLEES {
+                                    status = StatusCode::UciStatusMulticastListFull;
+                                    update_status =
+                                        MulticastUpdateStatusCode::StatusErrorMulticastListFull;
+                                } else {
+                                    dst_addresses.push(MacAddress::Short(controlee.short_address));
+                                };
+                            } else {
+                                status = StatusCode::UciStatusAddressAlreadyPresent;
+                                update_status =
+                                    MulticastUpdateStatusCode::StatusErrorAddressAlreadyPresent;
+                            }
+                            controlee_status.push(ControleeStatus {
+                                mac_address: controlee.short_address,
+                                subsession_id: controlee.subsession_id,
+                                status: update_status,
+                            });
+                        });
+                    }
+                    UpdateMulticastListAction::Delete => {
+                        new_controlees.iter().for_each(|controlee| {
+                            let mut update_status =
+                                MulticastUpdateStatusCode::StatusOkMulticastListUpdate;
+                            if !dst_addresses.contains(&MacAddress::Short(controlee.short_address))
+                            {
+                                status = StatusCode::UciStatusAddressNotFound;
+                                update_status = MulticastUpdateStatusCode::StatusErrorKeyFetchFail;
+                            } else {
+                                dst_addresses.retain(|value| {
+                                    *value != MacAddress::Short(controlee.short_address)
+                                });
+                            }
+                            controlee_status.push(ControleeStatus {
+                                mac_address: controlee.short_address,
+                                subsession_id: controlee.subsession_id,
+                                status: update_status,
+                            });
+                        });
+                    }
                 }
+                self.app_config.number_of_controlees = dst_addresses.len();
+                self.app_config.dst_mac_addresses = dst_addresses.clone();
+                let tx = self.tx.clone();
+                tokio::spawn(async move {
+                    tx.send(
+                        SessionUpdateControllerMulticastListNtfBuilder {
+                            controlee_status,
+                            remaining_multicast_list_size: dst_addresses.len() as u8,
+                            session_token: session_id,
+                        }
+                        .build()
+                        .into(),
+                    )
+                    .await
+                    .unwrap()
+                });
+                status
             }
         };
         SessionUpdateControllerMulticastListRspBuilder { status }.build()
