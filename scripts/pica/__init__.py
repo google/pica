4769a4fdfef0f159e60fdb13c050e300e1d75f23
Henri Chataing <henrichataing@google.com>
2023-11-16 17:50:00 -0800

    Organize python helpers into a package
    - pica.Host:        helper class to manage an host connection to a pica UWB subsytem
    - pica.packets.uci: helper parsing definitions
    - console:          command line interface for controlling a host device
    - ranging_example:  script that runs a ranging test between two host devices

---
+++ scripts/pica/__init__.py
@@ -0,0 +1,110 @@
+
+import asyncio
+from typing import Union
+from .packets import uci
+
+class Host:
+    def __init__(self, reader, writer, mac_address: bytes):
+        self.reader = reader
+        self.writer = writer
+        self.control_queue = asyncio.Queue()
+        self.data_queue = asyncio.Queue()
+        self.mac_address = mac_address
+
+        loop = asyncio.get_event_loop()
+        self.reader_task = loop.create_task(self._read_packets())
+
+
+    @staticmethod
+    async def connect(address: str, port: int, mac_address: bytes) -> 'Host':
+        reader, writer = await asyncio.open_connection(address, port)
+        return Host(reader, writer, mac_address)
+
+    def disconnect(self):
+        self.writer.close()
+
+    async def _read_exact(self, expected_len: int) -> bytes:
+        """ Read an exact number of bytes from the socket.
+
+        Raises an exception if the socket gets disconnected."""
+        received = bytes()
+        while len(received) < expected_len:
+            chunk = await self.reader.read(expected_len - len(received))
+            received += chunk
+        return received
+
+    async def _read_packet(self) -> bytes:
+        """ Read a single UCI packet from the socket.
+
+        The packet is automatically re-assembled if segmented on
+        the UCI transport."""
+
+        complete_packet_bytes = bytes()
+
+        # Note on reassembly:
+        # For each segment of a Control Message, the
+        # header of the Control Packet SHALL contain the same MT, GID and OID
+        # values. It is correct to keep only the last header of the
+        # segmented packet.
+        while True:
+            # Read the common packet header.
+            header_bytes = await self._read_exact(4)
+            header = uci.PacketHeader.parse_all(header_bytes)
+
+            # Read the packet payload.
+            payload_bytes = await self._read_exact(header.payload_length)
+            complete_packet_bytes += payload_bytes
+
+            # Check the Packet Boundary Flag.
+            match header.pbf:
+                case uci.PacketBoundaryFlag.COMPLETE:
+                    return header_bytes + complete_packet_bytes
+                case uci.PacketBoundaryFlag.NOT_COMPLETE:
+                    pass
+
+    async def _read_packets(self):
+        """ Loop reading UCI packets from the socket.
+        Receiving packets are added to the control queue."""
+        try:
+            while True:
+                packet = await self._read_packet()
+                await self.control_queue.put(packet)
+        except Exception as exn:
+            print(f"reader task closed")
+
+    async def _recv_control(self) -> bytes:
+        return await self.control_queue.get()
+
+    async def send_control(self, packet: uci.ControlPacket):
+        # TODO packet fragmentation.
+        packet = bytearray(packet.serialize())
+        packet[3] = len(packet) - 4
+        self.writer.write(packet)
+
+    async def expect_control(
+        self, expected: Union[type, uci.ControlPacket], timeout: float = 1.0
+    ) -> uci.ControlPacket:
+        """Wait for a control packet being sent from the controller.
+
+        Raises ValueError if the packet is not well formatted.
+        Raises ValueError if the packet does not match the expected type or value.
+        Raises TimeoutError if no packet is received after `timeout` seconds.
+        Returns the received packet on success.
+        """
+
+        packet = await asyncio.wait_for(self._recv_control(), timeout=timeout)
+        received = uci.ControlPacket.parse_all(packet)
+
+        if isinstance(expected, type) and not isinstance(received, expected):
+            raise ValueError(
+                f"received unexpected packet {received.__class__.__name__},"
+                + f" expected {expected.__name__}"
+            )
+
+        if isinstance(expected, uci.ControlPacket) and received != expected:
+            raise ValueError(
+                f"received unexpected packet {received.__class__.__name__},"
+                + f" expected {expected.__class__.__name__}"
+            )
+
+        return received
---
+++ scripts/ranging_example.py
@@ -0,0 +1,281 @@
+#!/usr/bin/env python3
+
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import asyncio
+import argparse
+from pica import Host
+from pica.packets import uci
+
+async def controller(host: Host, peer: Host):
+    await host.expect_control(
+        uci.DeviceStatusNtf(device_state=uci.DeviceState.DEVICE_STATE_READY))
+
+    await host.send_control(
+        uci.DeviceResetCmd(reset_config=uci.ResetConfig.UWBS_RESET))
+
+    await host.expect_control(
+        uci.DeviceResetRsp(status=uci.StatusCode.UCI_STATUS_OK))
+
+    await host.send_control(
+        uci.SessionInitCmd(
+            session_id=0,
+            session_type=uci.SessionType.FIRA_RANGING_SESSION))
+
+    await host.expect_control(
+        uci.SessionInitRsp(
+            status=uci.StatusCode.UCI_STATUS_OK))
+
+    await host.expect_control(
+        uci.SessionStatusNtf(
+            session_token=0,
+            session_state=uci.SessionState.SESSION_STATE_INIT,
+            reason_code=0))
+
+    mac_address_mode = 0x0
+    ranging_duration = int(1000).to_bytes(4, byteorder='little')
+    device_role_initiator = bytes([0])
+    device_type_controller = bytes([1])
+    await host.send_control(
+        uci.SessionSetAppConfigCmd(
+            session_token=0,
+            tlvs=[
+                uci.AppConfigTlv(
+                    cfg_id=uci.AppConfigTlvType.DEVICE_ROLE,
+                    v=device_role_initiator),
+                uci.AppConfigTlv(
+                    cfg_id=uci.AppConfigTlvType.DEVICE_TYPE,
+                    v=device_type_controller),
+                uci.AppConfigTlv(
+                    cfg_id=uci.AppConfigTlvType.DEVICE_MAC_ADDRESS,
+                    v=host.mac_address),
+                uci.AppConfigTlv(
+                    cfg_id=uci.AppConfigTlvType.MAC_ADDRESS_MODE,
+                    v=bytes([mac_address_mode])),
+                uci.AppConfigTlv(
+                    cfg_id=uci.AppConfigTlvType.RANGING_DURATION,
+                    v=ranging_duration),
+                uci.AppConfigTlv(
+                    cfg_id=uci.AppConfigTlvType.NO_OF_CONTROLEE,
+                    v=bytes([1])),
+                uci.AppConfigTlv(
+                    cfg_id=uci.AppConfigTlvType.DST_MAC_ADDRESS,
+                    v=peer.mac_address),
+            ]))
+
+    await host.expect_control(
+        uci.SessionSetAppConfigRsp(
+            status=uci.StatusCode.UCI_STATUS_OK,
+            cfg_status=[]))
+
+    await host.expect_control(
+        uci.SessionStatusNtf(
+            session_token=0,
+            session_state=uci.SessionState.SESSION_STATE_IDLE,
+            reason_code=0))
+
+    await host.send_control(
+        uci.SessionStartCmd(
+            session_id=0))
+
+    await host.expect_control(
+        uci.SessionStartRsp(
+            status=uci.StatusCode.UCI_STATUS_OK))
+
+    await host.expect_control(
+        uci.SessionStatusNtf(
+            session_token=0,
+            session_state=uci.SessionState.SESSION_STATE_ACTIVE,
+            reason_code=0))
+
+    await host.expect_control(
+        uci.DeviceStatusNtf(
+            device_state=uci.DeviceState.DEVICE_STATE_ACTIVE))
+
+    for n in range(1, 3):
+        event = await host.expect_control(
+            uci.ShortMacTwoWaySessionInfoNtf,
+            timeout=2.0)
+        event.show()
+
+    await host.send_control(
+        uci.SessionStopCmd(
+            session_id=0))
+
+    await host.expect_control(
+        uci.SessionStopRsp(
+            status=uci.StatusCode.UCI_STATUS_OK))
+
+    await host.expect_control(
+        uci.SessionStatusNtf(
+            session_token=0,
+            session_state=uci.SessionState.SESSION_STATE_IDLE,
+            reason_code=0))
+
+    await host.expect_control(
+        uci.DeviceStatusNtf(
+            device_state=uci.DeviceState.DEVICE_STATE_READY))
+
+
+async def controlee(host: Host, peer: Host):
+    await host.expect_control(
+        uci.DeviceStatusNtf(device_state=uci.DeviceState.DEVICE_STATE_READY))
+
+    await host.send_control(
+        uci.DeviceResetCmd(reset_config=uci.ResetConfig.UWBS_RESET))
+
+    await host.expect_control(
+        uci.DeviceResetRsp(status=uci.StatusCode.UCI_STATUS_OK))
+
+    await host.send_control(
+        uci.SessionInitCmd(
+            session_id=0,
+            session_type=uci.SessionType.FIRA_RANGING_SESSION))
+
+    await host.expect_control(
+        uci.SessionInitRsp(
+            status=uci.StatusCode.UCI_STATUS_OK))
+
+    await host.expect_control(
+        uci.SessionStatusNtf(
+            session_token=0,
+            session_state=uci.SessionState.SESSION_STATE_INIT,
+            reason_code=0))
+
+    mac_address_mode = 0x0
+    ranging_duration = int(1000).to_bytes(4, byteorder='little')
+    device_role_responder = bytes([1])
+    device_type_controlee = bytes([0])
+    await host.send_control(
+        uci.SessionSetAppConfigCmd(
+            session_token=0,
+            tlvs=[
+                uci.AppConfigTlv(
+                    cfg_id=uci.AppConfigTlvType.DEVICE_ROLE,
+                    v=device_role_responder),
+                uci.AppConfigTlv(
+                    cfg_id=uci.AppConfigTlvType.DEVICE_TYPE,
+                    v=device_type_controlee),
+                uci.AppConfigTlv(
+                    cfg_id=uci.AppConfigTlvType.DEVICE_MAC_ADDRESS,
+                    v=host.mac_address),
+                uci.AppConfigTlv(
+                    cfg_id=uci.AppConfigTlvType.MAC_ADDRESS_MODE,
+                    v=bytes([mac_address_mode])),
+                uci.AppConfigTlv(
+                    cfg_id=uci.AppConfigTlvType.RANGING_DURATION,
+                    v=ranging_duration),
+                uci.AppConfigTlv(
+                    cfg_id=uci.AppConfigTlvType.NO_OF_CONTROLEE,
+                    v=bytes([1])),
+                uci.AppConfigTlv(
+                    cfg_id=uci.AppConfigTlvType.DST_MAC_ADDRESS,
+                    v=peer.mac_address),
+            ]))
+
+    await host.expect_control(
+        uci.SessionSetAppConfigRsp(
+            status=uci.StatusCode.UCI_STATUS_OK,
+            cfg_status=[]))
+
+    await host.expect_control(
+        uci.SessionStatusNtf(
+            session_token=0,
+            session_state=uci.SessionState.SESSION_STATE_IDLE,
+            reason_code=0))
+
+    await host.send_control(
+        uci.SessionStartCmd(
+            session_id=0))
+
+    await host.expect_control(
+        uci.SessionStartRsp(
+            status=uci.StatusCode.UCI_STATUS_OK))
+
+    await host.expect_control(
+        uci.SessionStatusNtf(
+            session_token=0,
+            session_state=uci.SessionState.SESSION_STATE_ACTIVE,
+            reason_code=0))
+
+    await host.expect_control(
+        uci.DeviceStatusNtf(
+            device_state=uci.DeviceState.DEVICE_STATE_ACTIVE))
+
+    for n in range(1, 3):
+        event = await host.expect_control(
+            uci.ShortMacTwoWaySessionInfoNtf,
+            timeout=2.0)
+        event.show()
+
+    await host.send_control(
+        uci.SessionStopCmd(
+            session_id=0))
+
+    await host.expect_control(
+        uci.SessionStopRsp(
+            status=uci.StatusCode.UCI_STATUS_OK))
+
+    await host.expect_control(
+        uci.SessionStatusNtf(
+            session_token=0,
+            session_state=uci.SessionState.SESSION_STATE_IDLE,
+            reason_code=0))
+
+    await host.expect_control(
+        uci.DeviceStatusNtf(
+            device_state=uci.DeviceState.DEVICE_STATE_READY))
+
+
+async def run(address: str, uci_port: int, http_port: int):
+    try:
+        host0 = await Host.connect(address, uci_port, bytes([0, 1]))
+        host1 = await Host.connect(address, uci_port, bytes([0, 2]))
+    except Exception as exn:
+        print(
+            f'Failed to connect to Pica server at address {address}:{uci_port}\n' +
+            'Make sure the server is running')
+        exit(1)
+
+    async with asyncio.TaskGroup() as tg:
+        task0 = tg.create_task(controller(host0, host1))
+        task1 = tg.create_task(controlee(host1, host0))
+
+    host0.disconnect()
+    host1.disconnect()
+
+    print('Ranging test completed')
+
+
+def main():
+    """Start a Pica interactive console."""
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--address',
+                        type=str,
+                        default='127.0.0.1',
+                        help='Select the pica server address')
+    parser.add_argument('--uci-port',
+                        type=int,
+                        default=7000,
+                        help='Select the pica TCP UCI port')
+    parser.add_argument('--http-port',
+                        type=int,
+                        default=3000,
+                        help='Select the pica HTTP port')
+    asyncio.run(run(**vars(parser.parse_args())))
+
+
+if __name__ == '__main__':
+    main()
